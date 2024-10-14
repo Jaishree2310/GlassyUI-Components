1f55bd6a541068acbf059d257d2a67a8c9cd3c79
noob-coder-19 <ayushabhishekkumar19@gmail.com>
2024-10-14 22:15:48 +0530

    Resolved merge conflict for glassyuicomponentspage

--- src/components/GlassyUIComponentsPage.tsx
+++ src/components/GlassyUIComponentsPage.tsx
@@ -17,17 +17,13 @@ import {
   Layout,
   AlignLeft,
   ArrowUp,
-<<<<<<< HEAD
   DollarSign,
-=======
->>>>>>> main
 } from 'lucide-react';
 import Tooltip from './Tooltip';
 import SpeedDial from './SpeedDial';
 import BackToTopButton from './BackToTop';
 import { HiOutlineWrenchScrewdriver } from 'react-icons/hi2';
 import Accordion from './Accordion';
-
 
 const GlassyUIComponentsPage: React.FC = () => {
   const navigate = useNavigate();
@@ -54,18 +50,18 @@ const GlassyUIComponentsPage: React.FC = () => {
   };
 
   return (
-    <div className='min-h-screen font-sans bg-gradient-to-br from-gray-800 via-gray-900 to-black text-white'>
+    <div className='min-h-screen font-sans text-white bg-gradient-to-br from-gray-800 via-gray-900 to-black'>
       <BackToTopButton />
-      <div className='container mx-auto px-4 py-8 lg:py-12'>
-        <header className='flex justify-between items-center mb-16'>
+      <div className='container px-4 py-8 mx-auto lg:py-12'>
+        <header className='flex items-center justify-between mb-16'>
           <div
-            className='text-3xl lg:text-4xl font-bold tracking-tight cursor-pointer hover:text-pink-200 transition-colors duration-300'
+            className='text-3xl font-bold tracking-tight transition-colors duration-300 cursor-pointer lg:text-4xl hover:text-pink-200'
             onClick={navigateToLandingPage}
           >
             GlassyUI
           </div>
           <div className='flex gap-4'>
-            <div className='hidden md:flex items-center space-x-4'>
+            <div className='items-center hidden space-x-4 md:flex'>
               <a
                 href='/generator'
                 rel='noopener noreferrer'
@@ -77,7 +73,7 @@ const GlassyUIComponentsPage: React.FC = () => {
                 </span>
               </a>
             </div>
-            <div className='hidden md:flex items-center space-x-4'>
+            <div className='items-center hidden space-x-4 md:flex'>
               <a
                 href={githubRepoUrl}
                 target='_blank'
@@ -95,7 +91,7 @@ const GlassyUIComponentsPage: React.FC = () => {
         </header>
 
         {isMenuOpen && (
-          <div className='md:hidden mb-8'>
+          <div className='mb-8 md:hidden'>
             <a
               href={githubRepoUrl}
               target='_blank'
@@ -109,16 +105,16 @@ const GlassyUIComponentsPage: React.FC = () => {
         )}
 
         <main>
-          <h1 className='text-4xl lg:text-6xl font-extrabold mb-6 bg-clip-text text-transparent bg-gradient-to-r from-white to-pink-200'>
+          <h1 className='mb-6 text-4xl font-extrabold text-transparent lg:text-6xl bg-clip-text bg-gradient-to-r from-white to-pink-200'>
             Glassmorphic Components
           </h1>
-          <p className='text-lg lg:text-xl mb-12 max-w-2xl leading-relaxed'>
+          <p className='max-w-2xl mb-12 text-lg leading-relaxed lg:text-xl'>
             Elevate your UI with our collection of beautifully crafted,
             glassmorphic components. Perfect for creating modern, sleek
             interfaces with depth and style.
           </p>
 
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8'>
+          <div className='grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-3'>
             <ComponentCard
               title='Toast'
               description='Glassmorphic Toast Component. Click the button below to try it out!'
@@ -138,7 +134,7 @@ const GlassyUIComponentsPage: React.FC = () => {
               icon={<Sliders size={24} />}
               onClick={() => navigate('/slider-details')}
             >
-              <div className='flex flex-col space-y-4 mt-4'>
+              <div className='flex flex-col mt-4 space-y-4'>
                 <input
                   type='range'
                   min='0'
@@ -207,7 +203,7 @@ const GlassyUIComponentsPage: React.FC = () => {
               icon={<Box size={24} />}
               onClick={() => navigate('/button-details')}
             >
-              <div className='flex space-x-2 mt-4'>
+              <div className='flex mt-4 space-x-2'>
                 <button
                   className={`${getGlassyClasses()} px-4 py-2 hover:bg-white/20`}
                 >
@@ -254,7 +250,7 @@ const GlassyUIComponentsPage: React.FC = () => {
             >
               <div className={`${getGlassyClasses()} w-full h-4 mt-4`}>
                 <div
-                  className='bg-white/30 h-full rounded-2xl'
+                  className='h-full bg-white/30 rounded-2xl'
                   style={{ width: '60%' }}
                 ></div>
               </div>
@@ -286,13 +282,13 @@ const GlassyUIComponentsPage: React.FC = () => {
               <nav
                 className={`${getGlassyClasses()} flex justify-around mt-4 py-2`}
               >
-                <a href='#' className='hover:bg-white/20 px-2 py-1 rounded'>
+                <a href='#' className='px-2 py-1 rounded hover:bg-white/20'>
                   Home
                 </a>
-                <a href='#' className='hover:bg-white/20 px-2 py-1 rounded'>
+                <a href='#' className='px-2 py-1 rounded hover:bg-white/20'>
                   About
                 </a>
-                <a href='#' className='hover:bg-white/20 px-2 py-1 rounded'>
+                <a href='#' className='px-2 py-1 rounded hover:bg-white/20'>
                   Contact
                 </a>
               </nav>
@@ -315,14 +311,14 @@ const GlassyUIComponentsPage: React.FC = () => {
               </button>
               {isPopupOpen && (
                 <div
-                  className='fixed inset-0 flex items-center justify-center z-50'
+                  className='fixed inset-0 z-50 flex items-center justify-center'
                   onClick={() => setIsPopupOpen(false)}
                 >
                   <div
                     className={`${getGlassyClasses()} p-6 max-w-sm mx-auto`}
                     onClick={e => e.stopPropagation()}
                   >
-                    <h3 className='text-xl font-bold mb-2'>Popup Title</h3>
+                    <h3 className='mb-2 text-xl font-bold'>Popup Title</h3>
                     <p className='mb-4'>
                       This is a sample popup with glassmorphism effect.
                     </p>
@@ -356,7 +352,7 @@ const GlassyUIComponentsPage: React.FC = () => {
               icon={<Info size={24} />}
               onClick={() => navigate('/tooltip-details')}
             >
-              <div className='mt-1 mb-4 flex justify-around'>
+              <div className='flex justify-around mt-1 mb-4'>
                 <Tooltip text='Tooltip' position='left'>
                   <button className={`${getGlassyClasses()} px-4 py-2`}>
                     Left
@@ -381,9 +377,11 @@ const GlassyUIComponentsPage: React.FC = () => {
               icon={<ArrowUp size={24} />}
               onClick={() => navigate('/back-to-top-details')}
             >
-              <div className='mt-1 mb-4 flex justify-around'>
+              <div className='flex justify-around mt-1 mb-4'>
                 <button
-                  onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}
+                  onClick={() =>
+                    window.scrollTo({ top: 0, behavior: 'smooth' })
+                  }
                   className={`${getGlassyClasses()} px-4 py-2`}
                 >
                   Back to Top
@@ -392,26 +390,26 @@ const GlassyUIComponentsPage: React.FC = () => {
             </ComponentCard>
 
             <ComponentCard
-              title="Pricing Plans"
-              description="Choose a pricing plan that suits your needs. Affordable and flexible."
+              title='Pricing Plans'
+              description='Choose a pricing plan that suits your needs. Affordable and flexible.'
               icon={<DollarSign size={24} />}
-              onClick={() => navigate("/pricing-details")}
+              onClick={() => navigate('/pricing-details')}
             >
-              <div className="mt-1 mb-4 flex justify-around">
-                <div className="text-center">
-                  <h3 className="text-lg font-semibold">Basic</h3>
+              <div className='flex justify-around mt-1 mb-4'>
+                <div className='text-center'>
+                  <h3 className='text-lg font-semibold'>Basic</h3>
                   <button className={`${getGlassyClasses()} px-4 py-2 mt-2`}>
                     Select
                   </button>
                 </div>
-                <div className="text-center">
-                  <h3 className="text-lg font-semibold">Standard</h3>
+                <div className='text-center'>
+                  <h3 className='text-lg font-semibold'>Standard</h3>
                   <button className={`${getGlassyClasses()} px-4 py-2 mt-2`}>
                     Select
                   </button>
                 </div>
-                <div className="text-center">
-                  <h3 className="text-lg font-semibold">Premium</h3>
+                <div className='text-center'>
+                  <h3 className='text-lg font-semibold'>Premium</h3>
                   <button className={`${getGlassyClasses()} px-4 py-2 mt-2`}>
                     Select
                   </button>
@@ -424,7 +422,7 @@ const GlassyUIComponentsPage: React.FC = () => {
               icon={<AlignLeft size={24} />}
               onClick={() => navigate('/dropdown-details')}
             >
-              <div className='relative p-4 rounded-lg shadow-lg bg-white/10 pt-0'>
+              <div className='relative p-4 pt-0 rounded-lg shadow-lg bg-white/10'>
                 <div
                   className='flex items-center mt-4 cursor-pointer rounded-3xl'
                   onClick={e => {
@@ -438,12 +436,12 @@ const GlassyUIComponentsPage: React.FC = () => {
                 </div>
 
                 {isPopupOpen2 && (
-                  <div className='absolute z-10 mt-1 w-full bg-gray-800 text-white rounded shadow-lg'>
+                  <div className='absolute z-10 w-full mt-1 text-white bg-gray-800 rounded shadow-lg'>
                     {['options1', 'options2', 'options3'].map(
                       (option, index) => (
                         <div
                           key={option}
-                          className='px-4 py-2 hover:bg-gray-700 cursor-pointer'
+                          className='px-4 py-2 cursor-pointer hover:bg-gray-700'
                           onClick={() => handleOptionSelect(option)}
                         >
                           {option}
@@ -461,7 +459,7 @@ const GlassyUIComponentsPage: React.FC = () => {
               icon={<Layout size={24} />}
               onClick={() => navigate('/authentication-card')}
             >
-              <div className='mt-1 mb-4 flex justify-around'>
+              <div className='flex justify-around mt-1 mb-4'>
                 <button className={`${getGlassyClasses()} px-4 py-2`}>
                   Login
                 </button>
@@ -474,7 +472,8 @@ const GlassyUIComponentsPage: React.FC = () => {
               title='Accordion'
               description='Accordion component with glassmorphic styling.'
               icon={<Layout size={24} />}
-              onClick={() => navigate('/accordion-details')}>
+              onClick={() => navigate('/accordion-details')}
+            >
               <Accordion
                 title='Accordion Title 1'
                 content='This is the content of the first accordion.'
@@ -509,20 +508,20 @@ const ComponentCard: React.FC<{
       onClick={onClick}
     >
       <div className='flex items-center mb-4'>
-        <div className='p-2 bg-white/20 rounded-lg mr-4'>{icon}</div>
+        <div className='p-2 mr-4 rounded-lg bg-white/20'>{icon}</div>
         <h3 className='text-xl font-bold'>{title}</h3>
         {status && (
-          <span className='ml-2 px-2 py-1 bg-green-200 text-green-700 text-xs font-medium rounded'>
+          <span className='px-2 py-1 ml-2 text-xs font-medium text-green-700 bg-green-200 rounded'>
             {status}
           </span>
         )}
       </div>
-      <p className='text-sm opacity-80 mb-4 flex-grow'>{description}</p>
+      <p className='flex-grow mb-4 text-sm opacity-80'>{description}</p>
       {children}
-      <div className='flex items-center text-sm font-medium text-pink-200 mt-4'>
-        <div className='flex justify-center items-center '>
+      <div className='flex items-center mt-4 text-sm font-medium text-pink-200'>
+        <div className='flex items-center justify-center '>
           <span>Learn more</span>
-          <ArrowRight className='ml-2 w-6 pt-1 group-hover:translate-x-1 transition-transform duration-300' />
+          <ArrowRight className='w-6 pt-1 ml-2 transition-transform duration-300 group-hover:translate-x-1' />
         </div>
       </div>
     </div>
