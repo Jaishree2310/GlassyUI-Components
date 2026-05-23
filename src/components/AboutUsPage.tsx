2098edf5f8b4cc653101f90df3482d35376ec12a
Jaishree Singh <jaishrees23102001@gmail.com>
2026-05-23 16:38:13 +0530

    fix: build issues resolved

--- src/components/AboutUsPage.tsx
+++ src/components/AboutUsPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import BackToTopButton from './BackToTop';
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa6';
 import './GlassyUILandingPage.css'; // For .animated-glossy-text
 
 const AboutUsPage: React.FC = () => {
